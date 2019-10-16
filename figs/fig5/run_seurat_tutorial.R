a7cbdeedf5f812a76c95beeb57e48a21d4fd0c03
markziemann <mark.ziemann@gmail.com>
2019-10-17 09:59:30 +1100

    updated the seurat data from v2 to v3.0

--- figs/fig5/run_seurat_tutorial.R
+++ figs/fig5/run_seurat_tutorial.R
@@ -1,7 +1,6 @@
-# Note: Much of the below code has been lifted from the Satija Lab Tutorial 
+# Note: Much of the below code has been lifted from the Satija Lab Tutorial version 3 2019-10-17
 # https://satijalab.org/seurat/immune_alignment.html
 # Which has been archive in wayback machine for posterity
-# https://web.archive.org/web/20190329092345/https://satijalab.org/seurat/immune_alignment.html
 
 library("Seurat")
 
@@ -14,180 +13,127 @@ stim.data <- read.table("immune_stimulated_expression_matrix.txt.gz",  sep = "\t
 ###############################################
 # Setup the Seurat objects
 ###############################################
+
 # Set up control object
-ctrl <- CreateSeuratObject(raw.data = ctrl.data, project = "IMMUNE_CTRL", min.cells = 5)
-ctrl@meta.data$stim <- "CTRL"
-ctrl <- FilterCells(ctrl, subset.names = "nGene", low.thresholds = 500, high.thresholds = Inf)
-ctrl <- NormalizeData(ctrl)
-ctrl <- ScaleData(ctrl, display.progress = F)
+ctrl <- CreateSeuratObject(counts = ctrl.data, project = "IMMUNE_CTRL", min.cells = 5)
+ctrl$stim <- "CTRL"
+ctrl <- subset(ctrl, subset = nFeature_RNA > 500)
+ctrl <- NormalizeData(ctrl, verbose = FALSE)
+ctrl <- FindVariableFeatures(ctrl, selection.method = "vst", nfeatures = 2000)
+
 # Set up stimulated object
-stim <- CreateSeuratObject(raw.data = stim.data, project = "IMMUNE_STIM", min.cells = 5)
-stim@meta.data$stim <- "STIM"
-stim <- FilterCells(stim, subset.names = "nGene", low.thresholds = 500, high.thresholds = Inf)
-stim <- NormalizeData(stim)
-stim <- ScaleData(stim, display.progress = F)
-
-# Gene selection for input to CCA
-ctrl <- FindVariableGenes(ctrl, do.plot = F)
-stim <- FindVariableGenes(stim, do.plot = F)
-g.1 <- head(rownames(ctrl@hvg.info), 1000)
-g.2 <- head(rownames(stim@hvg.info), 1000)
-genes.use <- unique(c(g.1, g.2))
-genes.use <- intersect(genes.use, rownames(ctrl@scale.data))
-genes.use <- intersect(genes.use, rownames(stim@scale.data))
+stim <- CreateSeuratObject(counts = stim.data, project = "IMMUNE_STIM", min.cells = 5)
+stim$stim <- "STIM"
+stim <- subset(stim, subset = nFeature_RNA > 500)
+stim <- NormalizeData(stim, verbose = FALSE)
+stim <- FindVariableFeatures(stim, selection.method = "vst", nfeatures = 2000)
 
 ###############################################
-# Perform a canonical correlation analysis (CCA)
+# Perform integration
 ###############################################
-immune.combined <- RunCCA(ctrl, stim, genes.use = genes.use, num.cc = 30)
-
-pdf("seurat_plots.pdf")
-
-# visualize results of CCA plot CC1 versus CC2 and look at a violin plot
-p1 <- DimPlot(object = immune.combined, reduction.use = "cca", group.by = "stim", 
-    pt.size = 0.5, do.return = TRUE)
-
-p2 <- VlnPlot(object = immune.combined, features.plot = "CC1", group.by = "stim", 
-    do.return = TRUE)
-
-plot_grid(p1, p2)
-
-PrintDim(object = immune.combined, reduction.type = "cca", dims.print = 1:2, 
-    genes.print = 10)
-
-p3 <- MetageneBicorPlot(immune.combined, grouping.var = "stim", dims.eval = 1:30, 
-    display.progress = FALSE)
-
-DimHeatmap(object = immune.combined, reduction.type = "cca", cells.use = 500, 
-    dim.use = 1:9, do.balanced = TRUE)
-
-###############################################
-# Align the CCA subspaces
-###############################################
-immune.combined <- AlignSubspace(immune.combined, reduction.type = "cca", grouping.var = "stim", 
-    dims.align = 1:20)
-
-p1 <- VlnPlot(object = immune.combined, features.plot = "ACC1", group.by = "stim", 
-    do.return = TRUE)
-p2 <- VlnPlot(object = immune.combined, features.plot = "ACC2", group.by = "stim", 
-    do.return = TRUE)
-plot_grid(p1, p2)
+immune.anchors <- FindIntegrationAnchors(object.list = list(ctrl, stim), dims = 1:20)
+immune.combined <- IntegrateData(anchorset = immune.anchors, dims = 1:20)
 
 ###############################################
 # Perform an integrated analysis
 ###############################################
+
+DefaultAssay(immune.combined) <- "integrated"
+
+# Run the standard workflow for visualization and clustering
+immune.combined <- ScaleData(immune.combined, verbose = FALSE)
+immune.combined <- RunPCA(immune.combined, npcs = 30, verbose = FALSE)
 # t-SNE and Clustering
-immune.combined <- RunTSNE(immune.combined, reduction.use = "cca.aligned", dims.use = 1:20, 
-    do.fast = T)
-immune.combined <- FindClusters(immune.combined, reduction.type = "cca.aligned", 
-    resolution = 0.6, dims.use = 1:20)
+immune.combined <- RunUMAP(immune.combined, reduction = "pca", dims = 1:20)
+immune.combined <- FindNeighbors(immune.combined, reduction = "pca", dims = 1:20)
+immune.combined <- FindClusters(immune.combined, resolution = 0.5)
+
 # Visualization
-p1 <- TSNEPlot(immune.combined, do.return = T, pt.size = 0.5, group.by = "stim")
-p2 <- TSNEPlot(immune.combined, do.label = T, do.return = T, pt.size = 0.5)
+p1 <- DimPlot(immune.combined, reduction = "umap", group.by = "stim")
+p2 <- DimPlot(immune.combined, reduction = "umap", label = TRUE)
 plot_grid(p1, p2)
+
+DimPlot(immune.combined, reduction = "umap", split.by = "stim")
 
 ###############################################
 # Identify conserved cell type markers
 ###############################################
-nk.markers <- FindConservedMarkers(immune.combined, ident.1 = 7, grouping.var = "stim", 
-    print.bar = FALSE)
+
+DefaultAssay(immune.combined) <- "RNA"
+nk.markers <- FindConservedMarkers(immune.combined, ident.1 = 7, grouping.var = "stim", verbose = FALSE)
 head(nk.markers)
 
-FeaturePlot(object = immune.combined, features.plot = c("CD3D", "SELL", "CREM", 
-    "CD8A", "GNLY", "CD79A", "FCGR3A", "CCL2", "PPBP"), min.cutoff = "q9", cols.use = c("lightgrey", 
-    "blue"), pt.size = 0.5)
+FeaturePlot(immune.combined, features = c("CD3D", "SELL", "CREM", "CD8A", "GNLY", "CD79A", "FCGR3A", 
+    "CCL2", "PPBP"), min.cutoff = "q9")
 
-new.ident <- c("CD14 Mono", "CD4 Naive T", "CD4 Memory T", "B", "CD16 Mono", 
-    "T activated", "CD8 T", "NK", "DC", "B activated", "Mk", "pDC", "Eryth")
-for (i in 0:12) {
-    immune.combined <- RenameIdent(object = immune.combined, old.ident.name = i, 
-        new.ident.name = new.ident[i + 1])
-}
+immune.combined <- RenameIdents(immune.combined, `0` = "CD14 Mono", `1` = "CD4 Naive T", `2` = "CD4 Memory T", 
+    `3` = "CD16 Mono", `4` = "B", `5` = "CD8 T", `6` = "T activated", `7` = "NK", `8` = "DC", `9` = "B Activated", 
+    `10` = "Mk", `11` = "pDC", `12` = "Eryth", `13` = "Mono/Mk Doublets")
 
-TSNEPlot(immune.combined, do.label = T, pt.size = 0.5)
+DimPlot(immune.combined, label = TRUE)
 
-immune.combined@ident <- factor(immune.combined@ident, levels = (c("pDC", "Eryth", 
-    "Mk", "DC", "CD14 Mono", "CD16 Mono", "B activated", "B", "CD8 T", "NK", 
-    "T activated", "CD4 Naive T", "CD4 Memory T")))
-markers.to.plot <- c("CD3D", "CREM", "HSPH1", "SELL", "GIMAP5", "CACYBP", "GNLY", 
-    "NKG7", "CCL5", "CD8A", "MS4A1", "CD79A", "MIR155HG", "NME1", "FCGR3A", 
-    "VMO1", "CCL2", "S100A9", "HLA-DQA1", "GPR183", "PPBP", "GNG11", "HBA2", 
-    "HBB", "TSPAN13", "IL3RA", "IGJ")
-sdp <- SplitDotPlotGG(immune.combined, genes.plot = rev(markers.to.plot), cols.use = c("blue", 
-    "red"), x.lab.rot = T, plot.legend = T, dot.scale = 8, do.return = T, grouping.var = "stim")
+Idents(immune.combined) <- factor(Idents(immune.combined), levels = c("Mono/Mk Doublets", "pDC", 
+    "Eryth", "Mk", "DC", "CD14 Mono", "CD16 Mono", "B Activated", "B", "CD8 T", "NK", "T activated", 
+    "CD4 Naive T", "CD4 Memory T"))
+markers.to.plot <- c("CD3D", "CREM", "HSPH1", "SELL", "GIMAP5", "CACYBP", "GNLY", "NKG7", "CCL5", 
+    "CD8A", "MS4A1", "CD79A", "MIR155HG", "NME1", "FCGR3A", "VMO1", "CCL2", "S100A9", "HLA-DQA1", 
+    "GPR183", "PPBP", "GNG11", "HBA2", "HBB", "TSPAN13", "IL3RA", "IGJ")
+DotPlot(immune.combined, features = rev(markers.to.plot), cols = c("blue", "red"), dot.scale = 8, 
+    split.by = "stim") + RotatedAxis()
 
-
-###############################################
-# Identify differential expressed genes across conditions
-###############################################
-
-LabelPoint <- function(plot, genes, exp.mat, adj.x.t = 0, adj.y.t = 0, adj.x.s = 0, 
-    adj.y.s = 0, text.size = 2.5, segment.size = 0.1) {
-    for (i in genes) {
-        x1 <- exp.mat[i, 1]
-        y1 <- exp.mat[i, 2]
-        plot <- plot + annotate("text", x = x1 + adj.x.t, y = y1 + adj.y.t, 
-            label = i, size = text.size)
-        plot <- plot + annotate("segment", x = x1 + adj.x.s, xend = x1, y = y1 + 
-            adj.y.s, yend = y1, size = segment.size)
-    }
-    return(plot)
-}
-
-LabelUR <- function(plot, genes, exp.mat, adj.u.t = 0.1, adj.r.t = 0.15, adj.u.s = 0.05, 
-    adj.r.s = 0.05, ...) {
-    return(LabelPoint(plot, genes, exp.mat, adj.y.t = adj.u.t, adj.x.t = adj.r.t, 
-        adj.y.s = adj.u.s, adj.x.s = adj.r.s, ...))
-}
-
-LabelUL <- function(plot, genes, exp.mat, adj.u.t = 0.1, adj.l.t = 0.15, adj.u.s = 0.05, 
-    adj.l.s = 0.05, ...) {
-    return(LabelPoint(plot, genes, exp.mat, adj.y.t = adj.u.t, adj.x.t = -adj.l.t, 
-        adj.y.s = adj.u.s, adj.x.s = -adj.l.s, ...))
-}
-
-t.cells <- SubsetData(immune.combined, ident.use = "CD4 Naive T", subset.raw = T)
-t.cells <- SetAllIdent(t.cells, id = "stim")
-avg.t.cells <- log1p(AverageExpression(t.cells, show.progress = FALSE))
+t.cells <- subset(immune.combined, idents = "CD4 Naive T")
+Idents(t.cells) <- "stim"
+avg.t.cells <- log1p(AverageExpression(t.cells, verbose = FALSE)$RNA)
 avg.t.cells$gene <- rownames(avg.t.cells)
 
-cd14.mono <- SubsetData(immune.combined, ident.use = "CD14 Mono", subset.raw = T)
-cd14.mono <- SetAllIdent(cd14.mono, id = "stim")
-avg.cd14.mono <- log1p(AverageExpression(cd14.mono, show.progress = FALSE))
+cd14.mono <- subset(immune.combined, idents = "CD14 Mono")
+Idents(cd14.mono) <- "stim"
+avg.cd14.mono <- log1p(AverageExpression(cd14.mono, verbose = FALSE)$RNA)
 avg.cd14.mono$gene <- rownames(avg.cd14.mono)
 
-cd4.naive.t <- SubsetData(immune.combined, ident.use = "CD4 Naive T", subset.raw = T)
-cd4.naive.t <- SetAllIdent(cd4.naive.t, id = "stim")
-
-
-
-genes.to.label1 = c("ISG15", "LY6E", "IFI6", "ISG20", "MX1")
-genes.to.label2 = c("IFIT2", "IFIT1")
-genes.to.label3 = c("CXCL10", "CCL8")
+genes.to.label = c("ISG15", "LY6E", "IFI6", "ISG20", "MX1", "IFIT2", "IFIT1", "CXCL10", "CCL8")
 p1 <- ggplot(avg.t.cells, aes(CTRL, STIM)) + geom_point() + ggtitle("CD4 Naive T Cells")
-p1 <- LabelUR(p1, genes = c(genes.to.label1, genes.to.label2), avg.t.cells, 
-    adj.u.t = 0.3, adj.u.s = 0.23)
-p1 <- LabelUL(p1, genes = genes.to.label3, avg.t.cells, adj.u.t = 0.5, adj.u.s = 0.4, 
-    adj.l.t = 0.25, adj.l.s = 0.25)
+p1 <- LabelPoints(plot = p1, points = genes.to.label, repel = TRUE)
 p2 <- ggplot(avg.cd14.mono, aes(CTRL, STIM)) + geom_point() + ggtitle("CD14 Monocytes")
-p2 <- LabelUR(p2, genes = c(genes.to.label1, genes.to.label3), avg.cd14.mono, 
-    adj.u.t = 0.3, adj.u.s = 0.23)
-p2 <- LabelUL(p2, genes = genes.to.label2, avg.cd14.mono, adj.u.t = 0.5, adj.u.s = 0.4, 
-    adj.l.t = 0.25, adj.l.s = 0.25)
+p2 <- LabelPoints(plot = p2, points = genes.to.label, repel = TRUE)
 plot_grid(p1, p2)
 
-immune.combined@meta.data$celltype.stim <- paste0(immune.combined@ident, "_", 
-    immune.combined@meta.data$stim)
-immune.combined <- StashIdent(immune.combined, save.name = "celltype")
-immune.combined <- SetAllIdent(immune.combined, id = "celltype.stim")
-b.interferon.response <- FindMarkers(immune.combined, ident.1 = "B_STIM", ident.2 = "B_CTRL", 
-    print.bar = FALSE)
-head(b.interferon.response, 15)
-
-FeatureHeatmap(immune.combined, features.plot = c("CD3D", "GNLY", "IFI6", "ISG15", 
-    "CD14", "CXCL10"), group.by = "stim", pt.size = 0.25, key.position = "top", 
-    max.exp = 3)
+immune.combined$celltype.stim <- paste(Idents(immune.combined), immune.combined$stim, sep = "_")
+immune.combined$celltype <- Idents(immune.combined)
+Idents(immune.combined) <- "celltype.stim"
 
 dev.off()
+
+###############################################
+# extract for each cell type
+###############################################
+b_cell <- FindMarkers(immune.combined, ident.1 = "B_STIM", ident.2 = "B_CTRL", verbose = FALSE)
+mk <- FindMarkers(immune.combined, ident.1 = "Mk_STIM", ident.2 = "Mk_CTRL", verbose = FALSE)
+pDC <- FindMarkers(immune.combined, ident.1 = "pDC_STIM", ident.2 = "pDC_CTRL", verbose = FALSE)
+eryth <- FindMarkers(immune.combined, ident.1 = "Eryth_STIM", ident.2 = "Eryth_CTRL", verbose = FALSE)
+DC <- FindMarkers(immune.combined, ident.1 = "DC_STIM", ident.2 = "DC_CTRL", verbose = FALSE)
+CD14_mono <- FindMarkers(immune.combined, ident.1 = "CD14 Mono_STIM", ident.2 = "CD14 Mono_CTRL", verbose = FALSE)
+CD16_mono <- FindMarkers(immune.combined, ident.1 = "CD16 Mono_STIM", ident.2 = "CD16 Mono_CTRL", verbose = FALSE)
+B_act <- FindMarkers(immune.combined, ident.1 = "B Activated_STIM", ident.2 = "B Activated_CTRL", verbose = FALSE)
+CD8_T <-FindMarkers(immune.combined, ident.1 = "CD8 T_STIM", ident.2 = "CD8 T_CTRL", verbose = FALSE)
+NK <-FindMarkers(immune.combined, ident.1 = "NK_STIM", ident.2 = "NK_CTRL", verbose = FALSE)
+T_act <-FindMarkers(immune.combined, ident.1 = "T activated_STIM", ident.2 = "T activated_CTRL", verbose = FALSE)
+CD4_nai<-FindMarkers(immune.combined, ident.1 = "CD4 Naive T_STIM", ident.2 = "CD4 Naive T_CTRL", verbose = FALSE)
+CD4_mem<-FindMarkers(immune.combined, ident.1 = "CD4 Memory T_STIM", ident.2 = "CD4 Memory T_CTRL", verbose = FALSE)
+
+tbl<-list("b_cell"=b_cell,
+"mk"=mk,
+"pDC"=pDC,
+"eryth"=eryth,
+"DC"=DC,
+"CD14_mono"=CD14_mono,
+"CD16_mono"=CD16_mono,
+"B_act"=B_act,
+"CD8_T"=CD8_T,
+"NK"=NK,
+"T_act"=T_act,
+"CD4_nai"=CD4_nai,
+"CD4_mem"=CD4_mem)
+
 save.image("seurat_data.RData")
 

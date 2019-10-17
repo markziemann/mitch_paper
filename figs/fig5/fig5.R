49ae653179f3e45798bee2bd5545fa009bfb5bf6
markziemann <mark.ziemann@gmail.com>
2019-10-17 15:37:43 +1100

    update to figures

--- figs/fig5/fig5.R
+++ figs/fig5/fig5.R
@@ -1,38 +1,18 @@
 library("Seurat")
 library("mitch")
+library("gplots")
 
 load("seurat_data.RData")
 
-#unique(immune.combined@meta.data$celltype.stim)
+# the tbl object contains all the DE contrasts for different cell types
+str(tbl)
 
-cd14.mono<-FindMarkers(immune.combined, ident.1 = "CD14 Mono_STIM", ident.2 = "CD14 Mono_CTRL", print.bar = FALSE)
-pdc<-FindMarkers(immune.combined, ident.1 = "pDC_STIM", ident.2 = "pDC_CTRL", print.bar = FALSE)
-cd4.mem <- FindMarkers(immune.combined, ident.1 = "CD4 Memory T_STIM", ident.2 = "CD4 Memory T_CTRL", print.bar = FALSE)
-t.act <- FindMarkers(immune.combined, ident.1 = "T activated_STIM", ident.2 = "T activated_CTRL", print.bar = FALSE)
-cd4.nat <- FindMarkers(immune.combined, ident.1 = "CD4 Naive T_STIM", ident.2 = "CD4 Naive T_CTRL", print.bar = FALSE)
-cd8.t <- FindMarkers(immune.combined, ident.1 = "CD8 T_STIM", ident.2 = "CD8 T_CTRL", print.bar = FALSE)
-mk <- FindMarkers(immune.combined, ident.1 = "Mk_STIM", ident.2 = "Mk_CTRL", print.bar = FALSE)
-b.act <- FindMarkers(immune.combined, ident.1 = "B activated_STIM", ident.2 = "B activated_CTRL", print.bar = FALSE)
-b <- FindMarkers(immune.combined, ident.1 = "B_STIM", ident.2 = "B_CTRL", print.bar = FALSE)
-dc <- FindMarkers(immune.combined, ident.1 = "DC_STIM", ident.2 = "DC_CTRL", print.bar = FALSE)
-cd16.mono <- FindMarkers(immune.combined, ident.1 = "CD16 Mono_STIM", ident.2 = "CD16 Mono_CTRL", print.bar = FALSE)
-nk <- FindMarkers(immune.combined, ident.1 = "NK_STIM", ident.2 = "NK_CTRL", print.bar = FALSE)
-eryth <- FindMarkers(immune.combined, ident.1 = "Eryth_STIM", ident.2 = "Eryth_CTRL", print.bar = FALSE)
-
-library("mitch")
-cell_type_de<-list("cd14.mono"=cd14.mono,"pdc"=pdc,"cd4.mem"=cd4.mem,"t.act"=t.act,
-  "cd4.nat"=cd4.nat,"cd8.t"=cd8.t,"mk.b"=mk,"b.act"=b.act,"b"=b,"dc"=dc,
-  "cd16.mono"=cd16.mono,"nk"=nk,"eryth"=eryth)
-
-x<-mitch_import(cell_type_de,DEtype="seurat")
+x<-mitch_import(tbl,DEtype="seurat")
 
 # get gene sets from Reactome
 download.file("https://reactome.org/download/current/ReactomePathways.gmt.zip","ReactomePathways.gmt.zip")
 unzip("ReactomePathways.gmt.zip")
 genesets<-gmt_import("ReactomePathways.gmt")
-
-# run mitch
-res<-mitch_calc(x,genesets,resrows=200,priority="significance")
 
 # define a scale bar function
 image.scale <- function(z, zlim, col = heat.colors(12),
@@ -72,20 +52,37 @@ breaks, horiz=TRUE, ylim=NULL, xlim=NULL, ...){
 
 # generate some plots
 pdf("fig5.pdf")
-xc<-cor(x,use="pairwise.complete.obs")
-heatmap(xc,scale="none")
+
+# first look at gene level DE correlation 
+xc<-cor(x,method="p",use="pairwise.complete.obs")
+warm_palette <- colorRampPalette(c("dark red","red", "orange", "yellow","white"))(n = 25)
+heatmap.2(xc,margin=c(20, 20),cexRow=0.8,trace="none",cexCol=0.8,col=warm_palette,scale="none",main="gene level DE Pearson correlation")
+xc<-cor(x,method="s",use="pairwise.complete.obs")
+heatmap.2(xc,margin=c(20, 20),cexRow=0.8,trace="none",cexCol=0.8,col=warm_palette,scale="none",main="gene level DE Spearman correlation")
+
+max(apply(xc,2,function(x) {  max(x[order(-x)][2:length(x)])      } ) )
+min(cx)
+#heatmap(xc,scale="none",main="gene level DE Pearson correlation")
+
 pal.1=colorRampPalette(c("red", "yellow","white"), space="rgb")
 breaks <- seq(min(xc), max(xc),length.out=25)
 image.scale(xc, col=pal.1(length(breaks)-1), breaks=breaks, horiz=TRUE)
 
+#exclude erythrocytes
+x<-x[,which(colnames(x)!="eryth")]
+
+# run mitch with significance prioritisation
+res<-mitch_calc(x,genesets,resrows=50,priority="significance")
+
+# create a barplot of DP numbers in each cell type
 par(mar=c(5,10,4,2))
-counts<-apply(res$manova_result[,17:29], 2 , function(x) { length(which(p.adjust(x) <0.05) ) } ) 
+counts<-apply(res$enrichment_result[,16:27], 2 , function(x) { length(which(p.adjust(x) <0.05) ) } ) 
 counts<-counts[order(-counts)]
-
 barplot( counts, horiz=T,las=1,xlab="no gene sets FDR<0.05")
-par(mai=c(1.02,0.82,0.82,0.42))
 
-sig<-res$manova_result[which(res$manova_result$p.adjustMANOVA<0.05),4:16]
+# histogram of gene sets by cell type
+par(mai=c(1.02,0.82,0.82,0.42))
+sig<-res$enrichment_result[which(res$enrichment_result$p.adjustMANOVA<0.05),4:15]
 sig<-sign(t(sig))
 neg<-as.vector(apply(sig,2,function(x) { length(which(x==-1)) } ))
 neg<-neg[order(neg)]
@@ -93,12 +90,58 @@ pos<-as.vector(apply(sig,2,function(x) { length(which(x==1)) } ))
 pos<-pos[order(-pos)]
 hist(pos,xlab="number of cell clusters that gene set s is positive",ylab="number of gene sets",main="Regulation of FDR<0.05 gene sets")
 
-sig<-res$manova_result[which(res$manova_result$p.adjustMANOVA<0.05),c(1,4:16)]
+# heatmap prioritise by significance
+res<-mitch_calc(x,genesets,resrows=50,priority="significance")
+sig <- res$enrichment_result[1:30,c(1,4:15)]
 rownames(sig)<-sig$set
 sig$set=NULL
 colnames(sig)<-gsub("s.","",colnames(sig))
 my_palette <- colorRampPalette(c("blue", "white", "red"))(n = 25)
-heatmap.2(as.matrix(sig),margin=c(5, 22),cexRow=0.65,trace="none",cexCol=0.8,col=my_palette,scale="none")
+heatmap.2(as.matrix(sig),margin=c(10, 22),cexRow=0.65,trace="none",cexCol=0.8,col=my_palette,scale="none",main="top sets by significance")
+
+# 25
+sig <- res$enrichment_result[1:25,c(1,4:15)]
+rownames(sig)<-sig$set
+sig$set=NULL
+colnames(sig)<-gsub("s.","",colnames(sig))
+my_palette <- colorRampPalette(c("blue", "white", "red"))(n = 25)
+heatmap.2(as.matrix(sig),margin=c(15, 22),cexRow=0.65,trace="none",cexCol=0.8,col=my_palette,scale="none",main="top sets by significance")
+
+
+# heatmap prioritise by effect
+res<-mitch_calc(x,genesets,resrows=50,priority="effect")
+sig <- res$enrichment_result[1:30,c(1,4:15)]
+rownames(sig)<-sig$set
+sig$set=NULL
+colnames(sig)<-gsub("s.","",colnames(sig))
+my_palette <- colorRampPalette(c("blue", "white", "red"))(n = 25)
+heatmap.2(as.matrix(sig),margin=c(10, 22),cexRow=0.65,trace="none",cexCol=0.8,col=my_palette,scale="none",main="top sets by effect")
+
+# 25
+sig <- res$enrichment_result[1:25,c(1,4:15)]
+rownames(sig)<-sig$set
+sig$set=NULL
+colnames(sig)<-gsub("s.","",colnames(sig))
+my_palette <- colorRampPalette(c("blue", "white", "red"))(n = 25)
+heatmap.2(as.matrix(sig),margin=c(15, 22),cexRow=0.65,trace="none",cexCol=0.8,col=my_palette,scale="none",main="top sets by effect")
+
+
+# heatmap prioritise by SD
+res<-mitch_calc(x,genesets,resrows=50,priority="SD")
+sig <- res$enrichment_result[1:30,c(1,4:15)]
+rownames(sig)<-sig$set
+sig$set=NULL
+colnames(sig)<-gsub("s.","",colnames(sig))
+my_palette <- colorRampPalette(c("blue", "white", "red"))(n = 25)
+heatmap.2(as.matrix(sig),margin=c(10, 22),cexRow=0.65,trace="none",cexCol=0.8,col=my_palette,scale="none",main="top sets by SD")
+
+# 25
+sig <- res$enrichment_result[1:25,c(1,4:15)]
+rownames(sig)<-sig$set
+sig$set=NULL
+colnames(sig)<-gsub("s.","",colnames(sig))
+my_palette <- colorRampPalette(c("blue", "white", "red"))(n = 25)
+heatmap.2(as.matrix(sig),margin=c(15, 22),cexRow=0.65,trace="none",cexCol=0.8,col=my_palette,scale="none",main="top sets by SD")
 
 dev.off()
 
